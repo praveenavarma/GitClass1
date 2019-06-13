82ddf295acb7c837a3fb81742cf22b889a7cb10e
praveenavarma <praveena1139@gmail.com>
2019-06-13 11:45:35 +0530

    this is my first git project

--- src/org/firstday/GitBaseClass.java
+++ src/org/firstday/GitBaseClass.java
@@ -1,5 +1,8 @@
 package org.firstday;
 
 public class GitBaseClass {
-
+public static void main(String[] args) {
+System.out.println("this is my first git project");	
+System.out.println("modified");
+}
 }
