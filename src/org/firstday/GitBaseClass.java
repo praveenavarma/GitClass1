a816e8ad5ef0a95595a752ff8c187673c52b25eb
praveenavarma <praveena1139@gmail.com>
2019-06-13 12:21:05 +0530

    already modified

--- src/org/firstday/GitBaseClass.java
+++ src/org/firstday/GitBaseClass.java
@@ -3,6 +3,6 @@ package org.firstday;
 public class GitBaseClass {
 public static void main(String[] args) {
 System.out.println("this is my first git project");	
-System.out.println("modified");
+System.out.println(" already modified");
 }
 }
