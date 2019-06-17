8a4d8d3bbb42d3876215f479f5e9f1e8764cdcd8
praveenavarma <praveena1139@gmail.com>
2019-06-17 13:46:25 +0530

    added

--- src/org/firstday/GitBaseClass.java
+++ src/org/firstday/GitBaseClass.java
@@ -2,9 +2,18 @@ package org.firstday;
 
 public class GitBaseClass {
 public static void main(String[] args) {
-System.out.println("this is my first git project");	
-System.out.println(" already modified");
-System.out.println("this is git program");
+	String rev="";
+	String s="mom";
+	for(int i=s.length()-1;i>=0;i--) {
+		rev=rev+s.charAt(i);
+	}
+	if(s.equals(rev)) {
+		System.out.println("string is palindrome");
+	}
+	else {
+		System.out.println("string is not palindrome");
+	}
+
 
 
 }
